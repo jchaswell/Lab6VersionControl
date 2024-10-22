e8f6586f9bce66a6b35fec173c71cb5565a18233
JC <jchaswell@sizzlecorp.com>
2024-10-21 21:16:23 -0400

    updated menu with error checking, and cleaner formatting

--- main.py
+++ main.py
@@ -8,22 +8,40 @@ def encode(input_num):
         encoded_num += str(encoded_digit)[-1]
     return int(encoded_num)
 
-
+def decode(encoded_num):
+    pass
 
 def main():
-
     while True:
-        menu_selection = int((input('Input 1 to enter a value, input 2 to decode value, 3 to exit: ')))
-        #could add error checking to confirm 8 digit string of only nums
-        if menu_selection == 1:
-            input_num = int(input('Enter a number: '))
-            encoded_num = encode(input_num)
-            print(f'Your encoded number is: {encoded_num}')
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        try:
+            menu_option = int(input("Please enter an option: "))
+        except ValueError:
+            print("Invalid input")
+            continue
+
+        if menu_option == 1:
+            input_num = (input("Please enter your password to encode:  "))
+            if len(input_num) == 8 and input_num.isdigit():
+                encoded_num = encode(input_num)
+                print(f"Your password has been encoded and stored!")
+            else:
+                print("Invalid input.")
+            print("")
+
+        elif menu_option == 2:
+            if len(str(encoded_num)) == 8 and str(encoded_num).isdigit():
+                input_num = decode(encoded_num)
+                print(f"The encoded password is {encoded_num}, and the original password is {input_num}.")
+            else:
+                print("Invalid input.")
             print()
-        if menu_selection == 2:
-            #TODO: call decode function here
-            pass
-        if menu_selection == 3:
+        elif menu_option == 3:
             break
 
 main()
